77e3d307ca9759c310e17e2767d3bac6bc70b31c
EDSON DAVID GARCIA CONTRERAS <edson.garcia@saluddigna.it>
2022-05-16 12:12:57 -0600

    add

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -76,7 +76,7 @@ const Work = () => {
                 Consumo de API
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://fervent-mestorf-567d9f.netlify.app/'>
+                <a href='https://glittery-marshmallow-528be7.netlify.app/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
